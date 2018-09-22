f10e30941eca9250beefba2f31179d63574ad572
Jantastication O <georginaoregx@gmail.com>
2018-09-22 21:39:01 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 
 def square_array(numbers)
   new_array = []
-  numbers.each do |integer|
-    new_array << integer ** 2   #each integer in the new_array will be a square value when called
+  numbers.each do |number|
+    new_array << number ** 2   #each integer in the new_array will be a square value when called
   end
   new_array
 end
